287c011ae9404b4a6113eef30d285fc2ae2a7940
Sarvesh <sarveshgond0000@gmail.com>
2024-01-09 17:01:46 +0530

    navigation screen added

---
+++ lib/core/navigation_screen.dart
@@ -0,0 +1,83 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:pyramid/views/explore_screen.dart';
+import 'package:pyramid/views/home_screen.dart';
+import 'package:pyramid/views/stats_screen.dart';
+import 'package:pyramid/views/user_profile.dart';
+
+class NavigationBottomBar extends StatefulWidget {
+  const NavigationBottomBar({super.key});
+
+  @override
+  State<NavigationBottomBar> createState() => _NavigationBottomBarState();
+}
+
+class _NavigationBottomBarState extends State<NavigationBottomBar> {
+  int selectedIndex = 0;
+  static const List<Widget> widgetOptions = <Widget>[
+    HomeScreen(),
+    ExploreScreen(),
+    StatsScreen(),
+    UserProfileScreen()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetOptions.elementAt(selectedIndex),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Color(0xFFF6F2F2),
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              tabBackgroundColor: Color(0xFFBBF246),
+              color: Colors.black,
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.rocket_launch_outlined,
+                  text: 'Explore',
+                ),
+                GButton(
+                  icon: Icons.analytics_outlined,
+                  text: 'Stats',
+                ),
+                GButton(
+                  icon: Icons.person_outline,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
