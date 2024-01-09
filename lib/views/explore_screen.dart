403d2073a7641c02d779d6ad53b1076421616247
Sarvesh <sarveshgond0000@gmail.com>
2024-01-09 17:02:15 +0530

    three more screens added

---
+++ lib/views/explore_screen.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({super.key});
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Text("Explore"),
+      ),
+    );
+  }
+}
