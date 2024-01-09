49df143ac294ff329f8c234bf1da8554d70962b2
Sarvesh <sarveshgond0000@gmail.com>
2024-01-09 17:20:09 +0530

    some const file added

---
+++ lib/common/const.dart
@@ -0,0 +1,4 @@
+List<String> imgLink = ["assets/images/leg.png", "assets/images/backward.png"];
+List<String> exerciseDuration = ["10 min", "5 min"];
+List<String> exerciseName = ["Leg Exercises", "Backward Lun"];
+List<String> exercisetype = ["Beginner", "Beginner"];
